da2d46b22b3f0e2f1fa3c9e48e86bce0542ab41b
Алексей Парасненко <aleksejparasnenko@MacBook-Pro-Aleksej.local>
2023-06-12 03:24:34 +0700

    zadacha6_sdelana

--- homeWork_zadacha6/Program.cs
+++ homeWork_zadacha6/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Clear();
+
+Console.Write("Введите любое целое число: ");
+int num1 = int.Parse(Console.ReadLine());
+
+int znach = num1%2;
+
+if (znach==0){
+    Console.WriteLine($"ваше число {num1} четное");
+}
+else Console.WriteLine($"выше число {num1} не является четным");
+
