4fa0f3324008c3c851e83f2131156f80caff1fc6
Алексей Парасненко <aleksejparasnenko@MacBook-Pro-Aleksej.local>
2023-06-12 03:40:50 +0700

    zadacha8_sdelana

--- homeWork_zadacha8/Program.cs
+++ homeWork_zadacha8/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+
+Console.Clear();
+
+Console.Write("Введите любое целое число: ");
+int num1 = int.Parse(Console.ReadLine());
+
+int count = 0;
+int znach;
+while (count < num1){
+
+    count++;
+    Console.WriteLine($"count = {count}");
+    
+    znach = count%2;
+
+    if (znach==0){
+        Console.WriteLine($"четное числа {count}");
+    }
+}
+
+
