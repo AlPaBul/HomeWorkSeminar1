854b8c03b061fd360793fd502f25f3d3f5bc71db
Алексей Парасненко <aleksejparasnenko@MacBook-Pro-Aleksej.local>
2023-06-12 03:06:43 +0700

    zadacha2_zdelana

--- homeWork_zadacha2/Program.cs
+++ homeWork_zadacha2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+
+Console.Write ("введите любое целое число num1: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write ("введите любое целое число num2: ");
+int num2 = int.Parse(Console.ReadLine());
+
+int max = num1;
+
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+
+Console.WriteLine($"Болешее из числа {num1} и {num2} является число {max}");
+
+
+
+
