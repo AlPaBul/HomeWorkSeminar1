08f0c1855930f099e2a990779aa082f829c1e8fa
Алексей Парасненко <aleksejparasnenko@MacBook-Pro-Aleksej.local>
2023-06-12 03:14:13 +0700

    zadacha4_zdelana

--- homeWork_zadacha4/Program.cs
+++ homeWork_zadacha4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+
+Console.Write ("введите любое целое число num1: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write ("введите любое целое число num2: ");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.Write ("введите любое целое число num3: ");
+int num3 = int.Parse(Console.ReadLine());
+
+int max = num1;
+
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.WriteLine($"Болешим из чисел {num1}, {num2}, {num3} является число: {max}");
